ee41103c7f3e3a8bfa52577f392008be768180fe
Eugene Goh <gzkgt11@gmail.com>
2021-05-01 13:41:29 +0800

    Added Body

---
+++ src/components/Body/Body.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Body.css";
+
+function Body() {
+  return (
+    <div className="heading">
+      <h1>Hello world! I'm a Quote Generator.</h1>
+    </div>
+  );
+}
+
+export default Body;
