db97ba4504eb45d7b3c3cdc05007f82b71fa5c58
Eugene Goh <gzkgt11@gmail.com>
2021-05-01 13:41:40 +0800

    Added App

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Body from "../Body/Body";
+import Card from "../Card/Card";
+
+function App() {
+  return (
+    <>
+      <Body />
+      <Card />
+    </>
+  );
+}
+
+export default App;
